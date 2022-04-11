d974d9e8677e0e536818925b9a2b90fd7313fc0c
maximebcqt <becquetmaxime2306@gmail.com>
2022-04-11 14:45:29 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'CRM';
+  public title: string = 'CRM';
+  //si on veut pas initialiser variable direct on peut faire :
+  // ( et en gros ca veut dire peut etre type string ou undefined)
+  public titleUndefined: string | undefined; // ou on peut faire : public titleUndefined!: string;
+  // ou declarer comme ca avec initialisation valeur dans le constructeur
+  public title2: string;
+  constructor() {
+    this.title2 = 'titre2';
+  }
 }
