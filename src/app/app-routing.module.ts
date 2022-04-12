994537e49373ac03f1d97a37de7b7d3ef174834d
maximebcqt <becquetmaxime2306@gmail.com>
2022-04-12 14:54:11 +0200

    ajout routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ViewSignInComponent } from './login/views/view-sign-in/view-sign-in.component';
+import { ViewSignUpComponent } from './login/views/view-sign-up/view-sign-up.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
+  { path: 'sign-in', component: ViewSignInComponent },
+  { path: 'sign-up', component: ViewSignUpComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/ui/components/ui2/ui2.component.ts
+++ src/app/ui/components/ui2/ui2.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ui2',
   templateUrl: './ui2.component.html',
-  styleUrls: ['./ui2.component.scss']
+  styleUrls: ['./ui2.component.scss'],
 })
 export class Ui2Component implements OnInit {
+  public close: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.close = false;
   }
+  ngOnInit(): void {}
 
+  public toggle(): void {
+    this.close = !this.close;
+  }
 }
