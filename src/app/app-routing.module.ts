5144839a9ef09cd13334d904709196176cce2760
maximebcqt <becquetmaxime2306@gmail.com>
2022-04-12 15:13:05 +0200

    lazy loading et preload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { ViewSignInComponent } from './login/views/view-sign-in/view-sign-in.component';
 import { ViewSignUpComponent } from './login/views/view-sign-up/view-sign-up.component';
 
@@ -7,9 +7,16 @@ const routes: Routes = [
   { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
   { path: 'sign-in', component: ViewSignInComponent },
   { path: 'sign-up', component: ViewSignUpComponent },
+  {
+    path: 'orders',
+    loadChildren: () =>
+      import('./orders/orders.module').then((m) => m.OrdersModule),
+  }, // permet que sur le path orders, va charger orders en tache de fond
 ];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
