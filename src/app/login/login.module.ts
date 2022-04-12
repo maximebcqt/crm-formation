bf927e2b625d4322946c2453ac6df5253858daff
maximebcqt <becquetmaxime2306@gmail.com>
2022-04-12 09:16:57 +0200

    fix: creation ui compo + correction chemin

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,25 +1,18 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
 import { LoginRoutingModule } from './login-routing.module';
-import { ViewSignInComponent } from './views/view-sign-in/view-sign-in.component';
-import { ViewSignUComponent } from './views/view-sign-u/view-sign-u.component';
-import { ViewSignUpComponent } from './views/view-sign-up/view-sign-up.component';
-import { ViewResetPasswordComponent } from './views/view-reset-password/view-reset-password.component';
 import { ViewForgotPasswordComponent } from './views/view-forgot-password/view-forgot-password.component';
-
+import { ViewResetPasswordComponent } from './views/view-reset-password/view-reset-password.component';
+import { ViewSignInComponent } from './views/view-sign-in/view-sign-in.component';
+import { ViewSignUpComponent } from './views/view-sign-up/view-sign-up.component';
 
 @NgModule({
   declarations: [
     ViewSignInComponent,
-    ViewSignUComponent,
     ViewSignUpComponent,
     ViewResetPasswordComponent,
-    ViewForgotPasswordComponent
+    ViewForgotPasswordComponent,
   ],
-  imports: [
-    CommonModule,
-    LoginRoutingModule
-  ]
+  imports: [CommonModule, LoginRoutingModule],
 })
-export class LoginModule { }
+export class LoginModule {}
