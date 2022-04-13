c919e0954c7ccba2cab53df94d9e78edf8026794
maximebcqt <becquetmaxime2306@gmail.com>
2022-04-13 15:31:16 +0200

    add btn et table light + add valeur table

--- src/app/orders/views/view-list-orders/view-list-orders.component.ts
+++ src/app/orders/views/view-list-orders/view-list-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/core/models/order';
 import { OrdersService } from '../../services/orders.service';
 
 @Component({
@@ -8,12 +9,24 @@ import { OrdersService } from '../../services/orders.service';
 })
 export class ViewListOrdersComponent implements OnInit {
   public myTitle: string = 'titre list order';
-
+  public headers: string[] = [
+    'Type',
+    'Client',
+    'nbJours',
+    'tjmHT',
+    'Total HT',
+    'Total TTC',
+    'State',
+  ];
+  public collections!: Order[];
   constructor(private ordersService: OrdersService) {
-    this.ordersService.collection$.subscribe((dataRecup) =>
-      console.log(dataRecup)
-    );
+    this.ordersService.collection$.subscribe((dataRecup) => {
+      console.log(dataRecup);
+      this.collections = dataRecup;
+    });
   }
-
+  public changeTitle() {
+    this.myTitle = 'changement du titre ';
+  }
   ngOnInit(): void {}
 }
