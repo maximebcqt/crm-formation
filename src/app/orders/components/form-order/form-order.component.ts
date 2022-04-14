762922c789abe9eb60ebb6b2328f8645557395ff
maximebcqt <becquetmaxime2306@gmail.com>
2022-04-14 15:26:08 +0200

    add form formbuilder et val init

--- src/app/orders/components/form-order/form-order.component.ts
+++ src/app/orders/components/form-order/form-order.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { StateOrder } from 'src/app/core/enums/state-order';
 import { Order } from 'src/app/core/models/order';
 
@@ -10,10 +11,21 @@ import { Order } from 'src/app/core/models/order';
 export class FormOrderComponent implements OnInit {
   @Input() public init!: Order;
   public states: string[];
-
-  constructor() {
+  public formOrder!: FormGroup;
+  constructor(private FormBuilder: FormBuilder) {
     this.states = Object.values(StateOrder);
   }
-
-  ngOnInit(): void {}
+  // on initialise ici les valeur du form
+  ngOnInit(): void {
+    this.formOrder = this.FormBuilder.group({
+      tjmHt: [this.init.tjmHt],
+      nbJours: [this.init.nbJours],
+      tva: [this.init.tva],
+      state: [this.init.state],
+      typePresta: [this.init.typePresta],
+      client: [this.init.client],
+      comment: [this.init.comment],
+      id: [this.init.id],
+    });
+  }
 }
--- src/app/orders/views/view-add-order/view-add-order.component.ts
+++ src/app/orders/views/view-add-order/view-add-order.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/core/models/order';
 
 @Component({
   selector: 'app-view-add-order',
   templateUrl: './view-add-order.component.html',
-  styleUrls: ['./view-add-order.component.scss']
+  styleUrls: ['./view-add-order.component.scss'],
 })
 export class ViewAddOrderComponent implements OnInit {
+  public item: Order = new Order();
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
